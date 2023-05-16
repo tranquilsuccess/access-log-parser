97652627fd66f04374ab6d921b088e0c4cee4c73
tranquilsuccess <tpavlova1988@gmail.com>
2023-05-17 01:51:12 +0300

    Задание по теме "Синтаксис языка" и "Базовые типы данных"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите число ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число: ");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число: ");
+        int secondNumber = new Scanner(System.in).nextInt();
+
+        int sum = firstNumber + secondNumber;
+        System.out.println("Сумма чисел: " + sum);
+
+        int difference = firstNumber - secondNumber;
+        System.out.println("Разность чисел: " + difference);
+
+        int productOfNumbers = firstNumber * secondNumber;
+        System.out.println("Произведение чисел: " + productOfNumbers);
+
+        double quotient = (double) firstNumber/secondNumber;
+        System.out.println("Частное: " + quotient);
     }
 }
